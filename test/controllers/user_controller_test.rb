e52a367bc12a1027995c25c8322d54f6a2a0cb80
Katarzyna Starachowicz <kstarachowicz@gmail.com>
2016-02-02 16:31:00 +0100

    Change new user for signup and create test for it

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class UserControllerTest < ActionController::TestCase
-  test "should get new" do
-    get :new
+  test "should get sign up" do
+    get :signup
     assert_response :success
+    assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
   end
-
 end

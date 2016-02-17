f9a97b100bcff230c28cee3ff7d9682abf41db3c
Katarzyna Starachowicz <kstarachowicz@gmail.com>
2016-02-17 12:34:44 +0100

    Forgetting users

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
-  # Returns the hash digest of the given string.
+  # Returns the hash digest of the given string
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
@@ -27,8 +27,13 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  # Returns true if the given token matches the digest.
+  # Returns true if the given token matches the digest
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end

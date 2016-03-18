bf9ec63b2aaae198241278b6b769c7aab7d69889
Katarzyna Starachowicz <kstarachowicz@gmail.com>
2016-03-18 12:56:15 +0100

    Add user microposts

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,12 @@
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['AKIAISHFXBS2SKFUCV5Q'],
+      :aws_secret_access_key => ENV['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'],
+      :region                 => 'eu-central-1'
+    }
+    config.fog_directory     =  ENV['agnieszka1']
+  end
+end

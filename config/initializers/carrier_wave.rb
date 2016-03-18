bc9e9768b3eb57f4b32fab26d54f777b48d805ea
Katarzyna Starachowicz <kstarachowicz@gmail.com>
2016-03-18 13:10:21 +0100

    Debug information for S3 one more time

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAISHFXBS2SKFUCV5Q'],
-      :aws_secret_access_key => ENV['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'],
-      :region                => ENV['eu-central-1']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :region                => ENV['S3_REGION']
     }
-    config.fog_directory     =  ENV['agnieszka1']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
